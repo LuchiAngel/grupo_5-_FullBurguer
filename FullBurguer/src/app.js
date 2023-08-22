f08a25ce40a584a6e3ed53de304ee3d963b036cf
Victoria Mulhall <vicky.mulhall84@gmail.com>
2023-08-22 18:26:00 -0300

    distribucion de vistas

--- FullBurguer/src/app.js
+++ FullBurguer/src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app= express();
-const mainRouter= require("./routes/mainRouter")
-const path= require('path')
+const app = express();
+const mainRouter = require("./routes/mainRouter")
+const path = require('path')
 
 app.use(express.static('public'))
 
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
 
-app.listen(3000, ()=>{
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/users'), path.join(__dirname, 'views/products')]);
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
     console.log("servidor corriendo en puerto:http://localhost:3000/index");
 })
 
-app.use("/",mainRouter)
+app.use("/", mainRouter)
