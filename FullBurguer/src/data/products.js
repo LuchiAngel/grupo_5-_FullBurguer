01fbc0a9f3e8e3b01829ac88aefc92b918a87c38
luchiangel <luchinico2010@gmail.com>
2023-08-26 11:54:50 -0300

    armando base de datos

---
+++ FullBurguer/src/data/products.js
@@ -0,0 +1,65 @@
+[
+    {
+     "id": 1,
+     "name": "Pollo Bacon",
+     "price": 2.200,
+     "discount": 5,
+     "category": "visited",
+     "description": "Medallón de pollo crispy con queso cheddar, cebolla, lechuga, tomate, nuestra salsa epecial, y por supuesto bien recargada del más rico bacon, papas regulares.",
+     "image": "PolloBacon.jpg"
+    },
+    {
+     "id": 2,
+     "name": "Mega Pampeano",
+     "price": 1900,
+     "discount": 3,
+     "category": "in-sale",
+     "description": "Medallón de 150 gr de pura carne, con queso cheddar, lomo en fetas y nuestra salsa epecial, papas regulares",
+     "image": "MegaPampeano.jpg"
+    },
+    {
+     "id": 3,
+     "name": "Mega Doble Angus",
+     "price": 2200,
+     "discount": 25,
+     "category": "in-sale",
+     "description": "Doble medallón de 200 gr de pura carne angus premium, con doble queso cheddar, lechuga, cebolla y tomate más nuestra súper salsa tártara, papas regulares",
+     "image": "DobleAngus.jpg"
+    },
+    {
+     "id": 4,
+     "name": "Pollo Deluxe",
+     "description": "Medallón de pollo crispy, con queso doble cheddar, cebolla, lechuga, tomate, nuestra salsa picante con pepinillos, papas regulares",
+     "price": 2400,
+     "discount": 10,
+     "image": "PolloDeluxe.jpg",
+     "category": "in-sale"
+    },
+    {
+     "id": 5,
+     "name": "Nuggets x10",
+     "description": "10 bocaditos del más delicioso pollo crispy, acompañado con la salsa que más te guste: cheddar, aioli, mostaza dulce, y papas regulares",
+     "price": 2500,
+     "discount": 20,
+     "image": "Nuggets.jpg",
+     "category": "in-sale"
+    },
+    {
+     "id": 6,
+     "name": "Cajita Feliz",
+     "description": "Hamburguesa de carne junior con queso cheddar, acompañada de papas fritas, gaseosa a elección y juguete de la última colección",
+     "price": 2200,
+     "discount": 25,
+     "image": "Cajita.jpg",
+     "category": "in-sale"
+    },
+    {
+     "id": 7,
+     "name": "Mega Cheddar Deluxe",
+     "description": "Para los amantes del cheddar: Doble medallón de 200 gr de pura carne, con delicioso baño de queso cheddar y lluvia de bacon, con más cheddar por dentro, papas regulares",
+     "price": 2700,
+     "discount": 5,
+     "image": "MegaCheddar.jpg",
+     "category": "in-sale"
+    }
+   ]
