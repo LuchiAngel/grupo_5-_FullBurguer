11c604bc5eb4abdf8fb5e8eb2e3bc26602781099
Victoria Mulhall <vicky.mulhall84@gmail.com>
2023-11-10 23:33:15 -0300

    inicio de carrito de compras, hasta clase 49, 1:25

---
+++ public/js/agregarCarrito.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+    const agregarBoton = document.querySelector(".addCart")
+   if(JSON.parse(localStorage.getItem('carrito'))== null){
+    localStorage.setItem('carrito', JSON.stringify([]));
+   }
+   let nombre= document.querySelector('#name');
+   let id= document.querySelector('#idProduct');
+   let descr= document.querySelector('#descr');
+   let img = document.querySelector('#img');
+   let price= document.querySelector('#price');
+
+    agregarBoton.addEventListener('click', function(){
+        const carrito = JSON.parse(localStorage.getItem('carrito'));
+        const producto ={
+            id: id.innerText,
+            nombre: nombre.innerText,
+            descr: descr.innerText,
+            price: parseFloat(price.innerText),
+            img: img.alt,
+        }
+        if( carrito.length > 0){
+            let productoCarrito = carrito.find(row=> row.id == producto.id);
+            if (productoCarrito){
+                productoCarrito.quantity +=1
+                productoCarrito.subtotal += producto.price 
+            }else{
+                producto.quantity = 1
+                producto.subtotal = producto.quantity * producto.price
+                carrito.push(producto);      
+            }
+
+        }else{
+            producto.quantity = 1
+            producto.subtotal = producto.quantity * producto.price
+            carrito.push(producto);
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        console.log(producto);
+    })
+
+
+
+
+}
---
+++ public/js/cart.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+    let displayCarrito = function () {
+        let carrito = JSON.parse(localStorage.getItem('carrito'));
+        let carritoConteiner = document.querySelector('.selected');
+        carritoConteiner.innerHTML = ' '
+        carrito.forEach(element => {
+            carritoConteiner.innerHTML += `
+            <article>
+            <ul> 
+                <li><img class= "fotoProducto" src="/images/${element.img}" alt=''></li>
+                <li><h4 class="nombreHamburguesa">${element.nombre}</h4></li>    
+                <li><p class="descripcionHamburguesa">${element.descr}</p></li>
+                <li class="mas"><button type="button"><i onClick='sumar(${element.id})' class="fa-solid fa-plus"></i></button></li>
+                <li><p class="cantidad">${element.quantity}</p></li>
+                <li class="menos"><button type="button"><i onClick='restar(${element.id})' class="fa-solid fa-minus"></i></button></li>
+                <li><p class="precioTotal">$${element.price * element.quantity}</p></li>
+            </ul>
+    </article>
+            `
+        });
+    }
+   
+    displayCarrito()
+    actualizarCarrito()
+
+}
+let actualizarCarrito = function () {
+    let productos = JSON.parse(localStorage.getItem('carrito'));
+    let suma = productos.reduce((acum, current) => acum + current.subtotal, 0);
+    let total = document.getElementById('total');
+    total.innerHTML = `TOTAL $${suma}`
+}
+let sumar = function (id) {
+    let productos = JSON.parse(localStorage.getItem('carrito'));
+    let producto = productos.find((row) => row.id == id);
+    producto.quantity += 1;
+    producto.subtotal = producto.quantity * producto.price;
+    localStorage.setItem('carrito', JSON.stringify(productos));
+    displayCarrito()
+    actualizarCarrito()
+};
+let restar = function (id) {
+    let productos = JSON.parse(localStorage.getItem('carrito'));
+    let producto = productos.find((row) => row.id == id);
+    producto.quantity -= 1;
+    producto.subtotal = producto.quantity * producto.price;
+    if( producto.quantity ==0){
+        borrar(id)
+        return
+    }
+    localStorage.setItem('carrito', JSON.stringify(productos));
+    displayCarrito()
+    actualizarCarrito()
+};
+let borrar = function (id) {
+    let productos = JSON.parse(localStorage.getItem('carrito'));
+    let productosFiltrados = productos.filter((row) => row.id != id);
+    localStorage.setItem('carrito', JSON.stringify(productosFiltrados));
+    displayCarrito()
+    actualizarCarrito()
+
+
+}
